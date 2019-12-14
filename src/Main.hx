8aaabba2a2b6504932c809d99cdd4ebbd41d66b3
Nir Lanka <nir@evildino.com>
2019-12-14 18:12:38 +0530

    Add relative path to test call

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,7 @@ import macromania.readers.FileIOFactory;
 class Main {
 
 	static function main() {
-	  	var fac = new FileIOFactory('/Users/nirmal/Dev/haxe-one/test');
+	  	var fac = new FileIOFactory('./test');
 	  	var io = fac.getNextReader();
 	  	io.read();
 	  	var code = io.getContent();
